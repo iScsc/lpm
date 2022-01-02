b21c5fada23b5240df2c0be2b6725abc2f92a0c5
Nicolas LESQUOY <74468941+nicolaslesquoy@users.noreply.github.com>
2022-01-02 22:23:27 +0100

    Mise à jour
    mainV2.py opérationnel et process.py nettoyé

--- LaTeX_Project_Manager/mainV2.py
+++ LaTeX_Project_Manager/mainV2.py
@@ -1,26 +1,23 @@
 # -*- coding: utf-8 -*-
-# Packages :
+##########################################################
+# PACKAGES
+##########################################################
+
 import os # Pour la manipulation des dossiers
 import shutil # Pour la manipulation des fichiers
 from datetime import datetime # Pour la création de l'historique
 import traceback # Pour la gestion des erreurs
-from pptx import Presentation # Pour la création du .pptx 
-# Code :
+from pptx import Presentation # Pour la création du .pptx
+##########################################################
+# CODE
+##########################################################
 # Définitions des variables globales :
-now = datetime.now() # current date and time
-date_time = now.strftime("%m/%d/%Y, %H:%M:%S") # Recherche de la date lors de la création de l'historique
-main_folder_path = "C:\\Users\\NIC\\Documents\\GitHub\\SUPAERO" # Dossier où sont stockés les projets et documents à créer --> USER INPUT
-source_path = "C:\\Users\\Lorinfo\\Documents\\GitHub\\LaTeX-Project-Manager\\LaTeX_Project_Manager\\source_TeX" # Dossier où sont stockés les fichiers sources TeX --> USER INPUT
-standard_classe = [
-    "book",
-    "article",
-    "report",
-    "letter",
-    "scrartcl",
-    "scrreprt",
-    "scrbook",
-    "scrlttr2",
-] # Classes "standards" LaTeX utilisées dans mes documents
+now = datetime.now() # Récupération de la date et de l'heure
+date_time = now.strftime("%m/%d/%Y, %H:%M:%S") # Mise en forme
+# Dossier où sont stockés les projets et documents à créer --> USER INPUT :
+main_folder_path = "C:\\Users\\Lorinfo\\Documents\\GitHub\\SUPAERO"
+# Dossier où sont stockés les fichiers sources TeX --> USER INPUT :
+source_path = "C:\\Users\\Lorinfo\\Documents\\GitHub\\LaTeX-Project-Manager\\LaTeX_Project_Manager\\source_TeX"
 ##########################################################
 # Définitions des fonctions utiles :
 # --------------------------------------------------------
@@ -33,7 +30,7 @@ def historique(folder):
     with open(fname, "w") as file:
         file.write("CREATION HISTORIQUE - INITIALISATION A {date}\n".format(date=date_time))
         file.write("===========================================================\n")
-
+# Ajout d'une entrée dans l'historique
 def new_entry(project_name,folder):
     """
     Ajout d'un élément à l'historique
@@ -43,6 +40,7 @@ def new_entry(project_name,folder):
         file.write("Ajout d'un élément à l'historique à {date}\n".format(date=date_time))
         file.write("--> CREATION DE {pname}\n".format(pname=project_name))
         file.write("-------------------------------------------\n")
+
 # Création du répertoire   
 def make_folder(cd,fdname):
     """
@@ -57,6 +55,7 @@ def make_folder(cd,fdname):
         print("Directory ",folder_name," Created ")
     except FileExistsError:
         print("Directory ",folder_name," already exists")
+
 # Récupération des fichiers sources 
 def copytex(fname,fdname):
     """
@@ -68,11 +67,7 @@ def copytex(fname,fdname):
     original = source_path + "\\{name}".format(name=fname)
     target = main_folder_path + "\\{fname}".format(fname=fdname) + "\\{name}".format(name=fname)
     shutil.copyfile(original, target)
-# Fonction modèle = pas utilisée ici
-def rename_file(fdname,cd,old_name,new_name):
-    path_old_name = fdname + "\\{cdname}".format(cdname=cd) + "\\{name}".format(name=old_name)
-    path_new_name = fdname + main_folder_path + "\\{cdname}".format(cdname=cd) + "\\{name}".format(name=new_name)
-    os.rename(path_old_name,path_new_name)
+
 # Création du .pptx
 def make_pptx(project_name):
     """
@@ -80,6 +75,7 @@ def make_pptx(project_name):
     les images du document
     project_name --> str ; nom du projet où on souhaite créer le fichier
     """
+    print("Fonction executée")
     prs = Presentation()
     title_slide_layout = prs.slide_layouts[0]
     slide = prs.slides.add_slide(title_slide_layout)
@@ -88,7 +84,7 @@ def make_pptx(project_name):
 
     title.text = project_name
     subtitle.text = "Illustrations"
-    prs.save(main_folder_path + "\\{0}".format(project_name) +"\\images" + "images.pptx")
+    prs.save(main_folder_path + "\\{0}".format(project_name) +"\\images" + "\\images.pptx")
     # On sauvegarde le document dans le folder 'images'.
 
 # Routine de creation
@@ -100,11 +96,13 @@ def create_project(project_name,classe,images=True,pptx=False):
     images --> bool, default = True ; indique s'il faut créer un dossier pour stocker des images
     pptx --> bool, default = False ; indique s'il faut créer dans 'images' un .pptx
     """
+    marker = True
     cd = main_folder_path # working directory où il faut importer les dossiers
     path = cd + "\\{fdname}".format(fdname=project_name) # Path du nouveau projet à setup
     new_entry(project_name,main_folder_path) # Ajout d'un élément à l'historique correspondant au nouveau projet
     list_file = os.listdir(source_path) # Liste des fichiers se trouvant dans le dossier source
     if classe == "fiche":
+        make_folder(cd,project_name)
         for file in list_file:
             if file.startswith("bristol") or file.startswith("mypackage"):
                 copytex(file,project_name)
@@ -126,7 +124,11 @@ def create_project(project_name,classe,images=True,pptx=False):
         for file in list_file:
             if file.startswith("glob") or file.startswith("standard"):
                     copytex(file,project_name)
-    else: # Sinon je veux un beamer
+            if images:
+                make_folder(path,"images")
+                if pptx:
+                    make_pptx(project_name)
+    elif classe == "beam":
         make_folder(cd,project_name)
         for file in list_file:
             if file.startswith("glob") or file.startswith("beam"):
@@ -134,34 +136,45 @@ def create_project(project_name,classe,images=True,pptx=False):
         if images:
             make_folder(path,"images")
             # Pas besoin de créer un fichier .pptx ici
+    else:
+        marker = False
+        print("Aucune correspondance trouvée. Aucun dossier généré.")
+    if marker:
+        try:
+            os.chdir(path) # Changement de working directory
+            # print("Current working directory: {0}".format(os.getcwd())) # Debug
+        except FileNotFoundError:
+            print("Le dossier {0} n'existe pas.".format(path))
+        except NotADirectoryError:
+            print("{0} n'est pas un dossier.".format(path))
+        except PermissionError:
+            print("Vous n'avez pas la permission de changer {0}".format(path))
+        list_file = os.listdir(os.getcwd())
+        # print(list_file) # Debug
+        if classe in ["book","standard","beam"]: # Pour fiche, pas besoin
+            # J'utilise ici mes conventions : elles sont prises en compte dans les fichiers .tex
+            os.rename("glob_macros.tex","macros.tex")
+            os.rename("glob_PackagesGlobStandard.tex","packages.tex")
+            os.rename("standard_snippet_template.tex","main.tex")
+        os.startfile(path) # Pour ouvrir quand il a été créé.
+    else:
+        print("Aucun dossier n'a été créé. Fin du programme.")
+
+def remove_project(project_name):
+    """
+    Routine de suppression d'un projet
+    project_name --> str ; nom du projet à supprimer.
+    """
+    delpath = main_folder_path + "\\{0}".format(project_name)
     try:
-        os.chdir(path) # Changement de working directory
-        # print("Current working directory: {0}".format(os.getcwd())) # Debug
-    except FileNotFoundError:
-        print("Le dossier {0} n'existe pas.".format(path))
-    except NotADirectoryError:
-        print("{0} n'est pas un dossier.".format(path))
-    except PermissionError:
-        print("Vous n'avez pas la permission de changer {0}".format(path))
-    list_file = os.listdir(os.getcwd())
-    # print(list_file) # Debug
-    if classe in ["book","standard","fiche"]:
-        # J'utilise ici mes conventions : elles sont prises en compte dans les fichiers .tex
-        os.rename("glob_macros.tex","macros.tex")
-        os.rename("glob_PackagesGlobStandard.tex","packages.tex")
-        os.rename("standard_snippet_template.tex","main.tex")
-    else: # Sinon c'est un beamer
-        os.rename("glob_macros.tex","macros.tex")
-        os.rename("glob_PackagesGlobStandard.tex","packages.tex")
-        os.rename("beam_snippet_template_beamer.tex","main.tex")
-    os.startfile(path) # Pour ouvrir quand il a été créé.
-
-
+        shutil.rmtree(delpath)
+    except:
+        traceback.print_exc() # Pour récuperer les erreurs et les informations correspondantes
 
 ##################################################################
 # INITIALISATION D'UN NOUVEAU PROJET
 # historique(main_folder_path)
 
-create_project("test","article",images=True,pptx=True)
+create_project("test11","book",images=True,pptx=True)
 #remove_project("test")
 
