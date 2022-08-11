abe6767c3b005649844c66cb2f789d592386f83b
Nicolas LESQUOY <nicolas.lesquoyu@gmail.com>
2022-08-11 11:43:14 +0200

    Nettoyage du code et réécriture en cours

--- src/SettingsClass.py
+++ src/SettingsClass.py
@@ -1,8 +1,20 @@
 # Settings.py - global variables
 import json
 
-with open("/home/nlesquoy/ghq/LaTeX-Project-Manager/settings/config.json","r") as ReadFile:
-    SettingsDict = json.load(ReadFile)
+class SettingsClass:
+    def __init__(self,PathToJSON):
+        self.PathToJSON = PathToJSON
+    
+    def GetSettings(self):
+        "Loads json"
+        with open(self.PathToJSON,"r") as ReadFile:
+            SettingsDict = json.load(ReadFile)
+        return SettingsDict
+    
+    def GetClassList(self):
+        "Get available classes"
+        SettingsDict = SettingsClass.GetSettings(self)
+        return [SettingsDict["latex_class"][i]["name"] for i in range(len(SettingsDict["latex_class"]))]
 
-PathToSource = SettingsDict["PathToSource"]
-ClassList = ["fiche","standard","beamer"]
+# test = SettingsClass(r"/home/nlesquoy/ghq/LaTeX-Project-Manager/settings/config.json")
+# print(SettingsClass.GetClassList(test))
